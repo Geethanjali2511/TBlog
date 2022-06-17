96d45169477b032ff2c4fb028d9d1cf625d56d46
Geethanjali <geeth@192.168.0.213>
2022-06-17 15:51:24 +0100

    test

---
+++ src/main/java/com/example/TBlog/TBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.TBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TBlogApplication.class, args);
+	}
+
+}
